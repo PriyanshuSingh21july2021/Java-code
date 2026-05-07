bd1d9ef588c7b5fdf4a7d312f774930864b3aab2
Priyanshu Kumar Singh <priyanshusingh88251@gmail.com>
2026-05-07 19:24:03 +0530

    Update Queues.java

--- Queues.java
+++ Queues.java
@@ -5,6 +5,8 @@ public class Queues {
 
    public static void main(String[] args) {
 
+      System.out.println("JAVA COLLECTION");
+
       Queue queue = new LinkedList<>();
 
       /* 1. Add() */
