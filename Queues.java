096644751f6dbe5253526a47563fe23db9f7233d
Priyanshu Kumar Singh <priyanshusingh88251@gmail.com>
2026-05-12 12:07:46 +0530

    Update Queues.java

--- Queues.java
+++ Queues.java
@@ -7,22 +7,18 @@ public class Queues {
 
       System.out.println("JAVA COLLECTION");
 
-      Queue queue = new LinkedList<>();
+      Queue<Integer> queue = new LinkedList<>();
 
-      /* 1. Add() */
       queue.add(5);
       queue.add(9);
       queue.add(7);
       queue.add(23);
+
       System.out.println(queue);
 
-      /* 2. remove() */
-      queue.remove();
-      System.out.println(queue);
+      // Intentional Error
+      int result = 10 / 0;
 
-      /* 3. */
-      queue.isEmpty();
-      System.out.println(queue);
-
+      System.out.println(result);
    }
 }
